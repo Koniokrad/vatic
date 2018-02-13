f4b267de98623052754d7d5d28a963a1a5f55cc0
Koniokrad <koniokradus@gmail.com>
2018-02-13 12:22:30 +0100

    updated sh file. necessary files are overwritten directly from the terminal

--- vatic.sh
+++ vatic.sh
@@ -5,14 +5,18 @@ sudo apt-get install -y git python-setuptools python-dev libfreetype6 libfreetyp
 sudo easy_install -U cython==0.20
 sudo easy_install -U SQLAlchemy wsgilog  mysql-python munkres parsedatetime argparse
 sudo easy_install -U numpy
+
+#add ffmpeg-3 repo for 16.04
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3
 sudo apt-get update
 sudo apt-get install ffmpeg
 
+#create new vatic directory
 cd ~/Desktop
 mkdir vatic
 cd vatic
 
+#clone repo from github
 git clone https://github.com/cvondrick/turkic.git
 git clone https://github.com/cvondrick/pyvision.git
 git clone https://github.com/cvondrick/vatic.git
@@ -27,34 +31,32 @@ cd ..
 
 sudo cp /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf.backup
 
-#change path in 000-default.conf 
-sudo nano /etc/apache2/sites-enabled/000-default.conf
-
 #change all data from 000-default.conf to below, make sure paths are correct
+sudo echo "
+   WSGIDaemonProcess www-data
+   WSGIProcessGroup www-data
 
-#    WSGIDaemonProcess www-data
-#   WSGIProcessGroup www-data
+    <VirtualHost *:80>
+        ServerName localhost
+        DocumentRoot /home/$USER/Desktop/vatic/vatic/public
 
-#    <VirtualHost *:80>
-#        ServerName localhost
-#        DocumentRoot /home/administrator/Desktop/vatic/vatic/public
-
-#        WSGIScriptAlias /server /home/administrator/Desktop/vatic/vatic/server.py
-#        CustomLog /var/log/apache2/access.log combined
-#    </VirtualHost>
-
-#change path in apache2.conf
-sudo nano /etc/apache2/apache2.conf
+        WSGIScriptAlias /server /home/$USER/Desktop/vatic/vatic/server.py
+        CustomLog /var/log/apache2/access.log combined
+    </VirtualHost>
+" | sudo tee /etc/apache2/sites-enabled/000-default.conf > /dev/null
 
 #copy all lines below into apache2.conf, make sure paths are correct and tabs are equal to other (see in nano editor)
-
-#<Directory /home/administrator/Desktop/vatic/vatic/>
-#        Options Indexes FollowSymLinks
-#        AllowOverride None
-#        Require all granted
-#</Directory>
+echo "
+<Directory /home/$USER/Desktop/vatic/vatic/>
+        Options Indexes FollowSymLinks
+        AllowOverride None
+        Require all granted
+</Directory>
+" | sudo tee -a /etc/apache2/apache2.conf > /dev/null
 
 sudo cp /etc/apache2/mods-available/headers.load /etc/apache2/mods-enabled
+
+#restart apache2
 sudo apache2ctl graceful
 
 printf "\n----------------------\n\n"
@@ -65,8 +67,7 @@ create database vatic;
 EOF
 
 #update vatic/vatic/config.py file with mysql pass
-
-#In the vatic/vatic directory:
+#In the vatic directory:
 cd vatic/
 cp config.py-example config.py
 sudo nano config.py
@@ -75,6 +76,7 @@ sudo apt-get install python-imaging
 turkic setup --database
 turkic setup --public-symlink
 
+#check if turkic installed succesfully
 printf "\n----------------------\n\n"
 turkic status --verify
 printf "\n----------------------\n\n"
